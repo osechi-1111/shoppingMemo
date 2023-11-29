a34822213372d0e51b7a6689cc66470350c2728a
osechi-1111 <121627718+osechi-1111@users.noreply.github.com>
2023-11-30 00:17:21 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'shoppingMemo';
+var urlsToCache = [
+  '/osechi-1111.github.io/shoppingMemo/'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
